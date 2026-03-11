fe08e1c30d9d6d0f8231fc749f2c89b0952c7b60
Dima <dima.stepanov20050909@gmail.ru>
2026-03-11 15:33:25 +0300

    Add widget module with mask_account_card and get_date functions

---
+++ tests/test_widget.py
@@ -0,0 +1,5 @@
+from src.widget import get_date, mask_account_card
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
