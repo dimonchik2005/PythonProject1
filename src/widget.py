3c4a30fa55bfd65ee59bb2db6be07199250d25d8
Dima <dima.stepanov20050909@gmail.ru>
2026-03-11 18:25:43 +0300

    Fix flake8 issues and add docstrings

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-
+# обрабатывает информацию как о картах, так и о счетах
 def mask_account_card(data: str) -> str:
     parts = data.split()
     number = parts[-1]
@@ -11,7 +11,8 @@ def mask_account_card(data: str) -> str:
 
     return f"{name} {get_mask_card_number(number)}"
 
-
+# ринимает на вход строку с датой в формате  "2024-03-11T02:26:18.671407"
+# и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
 def get_date(date_string: str) -> str:
     date_part = date_string[:10]
     year, month, day = date_part.split("-")
